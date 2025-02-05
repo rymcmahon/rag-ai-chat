2ba720e14765e4cce40ea56a285d136e6f2e0398
Ryan <rymcmahon@users.noreply.github.com>
2025-02-05 16:56:07 -0500

    use chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,4 @@
-class PromptsController < ApplicationController
+class ChatsController < ApplicationController
   def index
   end
 
@@ -8,7 +8,7 @@ class PromptsController < ApplicationController
 
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.append("prompt-messages", partial: "prompts/message", locals: { query: query, answer: answer })
+        render turbo_stream: turbo_stream.append("chat-messages", partial: "chats/message", locals: { query: query, answer: answer })
       end
       format.html { redirect_to root_path, notice: "Response received" }
     end
